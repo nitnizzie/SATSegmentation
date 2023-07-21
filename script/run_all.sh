54bfe7c310a71f9fcce29e7d21f3fb2fea9ec179
sklationd <sklationd@gmail.com>
2023-07-21 18:02:51 +0900

    script: colab

--- script/run_all.sh
+++ script/run_all.sh
@@ -1,6 +1,6 @@
 ############# run in single GPU ##############
-GPUS=(0 1 2 3)
-NUM_GPUS=4
+GPUS=(0)
+NUM_GPUS=1
 ##############################################
 i=0
 #LR="1e-3"
@@ -8,36 +8,10 @@ i=0
 LR="1e-3 5e-4 1e-4 5e-5 1e-5"
 EPOCH="100 200 400"
 
-
-
-wait_n() {
-  #limit the max number of jobs as NUM_MAX_JOB and wait
-  background=($(jobs -p))
-  local default_num_jobs=4 #12
-  local num_max_jobs=4
-  echo $num_max_jobs
-  if ((${#background[@]} >= num_max_jobs)); then
-    wait -n
-  fi
-}
-
 for lr in $LR; do
   for epoch in $EPOCH; do
     python train.py \
-          --gpu_idx ${GPUS[$i]} \
-          -m DeepLabV3 \
-          --preprocess_fn \
-          --transform 2 \
-          -lr "$lr" \
-          -ep "$epoch" \
-          --home '/content/drive/MyDrive/Dacon' \
-          2>&1 &
-    wait_n
-    # increment i
-    ((i=i+1))
-
-    python train.py \
-          --gpu_idx ${GPUS[$i]} \
+          --gpu_idx 0 \
           -m DeepLabV3 \
           --preprocess_fn \
           --transform 2 \
@@ -46,22 +20,5 @@ for lr in $LR; do
           -ep "$epoch" \
           --home '/content/drive/MyDrive/Dacon' \
           2>&1 &
-    wait_n
-    # increment i
-    ((i=i+1))
-  #
-    python train.py \
-          --gpu_idx ${GPUS[$i]} \
-          -m DeepLabV3 \
-          --loss_fn dice_v2 \
-          --preprocess_fn \
-          --transform 2 \
-          -lr "$lr" \
-          -ep "$epoch" \
-          --home '/content/drive/MyDrive/Dacon' \
-          2>&1 &
-    wait_n
-    # increment i
-    ((i=i+1))
   done
 done
