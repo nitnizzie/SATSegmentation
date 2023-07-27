783feade4acbfa3ef7d5ad49e3f13b6f95ebaf3d
sklationd <sklationd@gmail.com>
2023-07-27 20:13:41 +0900

    feat: test rotate

--- ensemble_test.py
+++ ensemble_test.py
@@ -17,16 +17,11 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--batch_size', type=int, default=16)
     parser.add_argument('-m', '--model', type=str, default="DeepLabV3",
         choices=["Unet", "Unet++", "FPN", "PSPNet", "DeepLabV3", "DeepLabV3+"])
-    parser.add_argument('--model_dir', nargs='+', help='Input strings to be stored in a list.')
-
-    # parser.add_argument('--model_dir_1', type=str, default=None)
-    # parser.add_argument('--model_dir_2', type=str, default=None)
+    parser.add_argument('--model_dir', type=str, default=None)
     parser.add_argument('--mask_ratio', type=float, default=0.35)
     args = parser.parse_args()
 
     time = datetime.now().strftime('%m_%d_%H:%M:%S')
-    # model_dir_1 = f'./models/{args.model_dir_1}'
-    # model_dir_2 = f'./models/{args.model_dir_2}'
 
     if args.model_dir:
         input_list = args.model_dir
@@ -42,80 +37,90 @@ if __name__ == '__main__':
 
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
-    transform = A.Compose(
+
+    transform_list = []
+    transform_list.append(A.Compose(
         [
             A.Resize(224, 224),
             A.Normalize(mean=mean, std=std, always_apply=True),
             A.pytorch.ToTensorV2(),
         ]
-    )
+    ))
+    transform_list.append(A.Compose(
+        [
+            A.Resize(224, 224),
+            # Rotate 90
+            A.Affine(rotate=90, always_apply=True),
+            A.Normalize(mean=mean, std=std, always_apply=True),
+            A.pytorch.ToTensorV2(),
+        ]
+    ))
+    transform_list.append(A.Compose(
+        [
+            A.Resize(224, 224),
+            # Rotate 180
+            A.Affine(rotate=180, always_apply=True),
+            A.Normalize(mean=mean, std=std, always_apply=True),
+            A.pytorch.ToTensorV2(),
+        ]
+    ))
+    transform_list.append(A.Compose(
+        [
+            A.Resize(224, 224),
+            # Rotate 270
+            A.Affine(rotate=270, always_apply=True),
+            A.Normalize(mean=mean, std=std, always_apply=True),
+            A.pytorch.ToTensorV2(),
+        ]
+    ))
 
-    test_dataset = SatelliteDataset(csv_file='./test.csv', transform=transform, infer=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+
+    test_dataset_list = []
+    for transform in transform_list:
+        test_dataset_list.append(SatelliteDataset(csv_file='./test.csv', transform=transform, infer=True))
+    
+    test_dataloader_list = []
+    for test_dataset in test_dataset_list:
+        test_dataloader_list.append(DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4))
 
     # model initialization
-
-    model_list = []
-    for model_dir in input_list:
-        model, _ = load_model(args.model)
+    model, _ = load_model(args.model)
+    if args.model_dir:
         model.load_state_dict(torch.load(model_dir))
-        model.to(device)
-        model_list.append(model)
-    #
-    # model_1, _ = load_model(args.model)
-    # if args.model_dir_1:
-    #     model_1.load_state_dict(torch.load(model_dir_1))
-    # model_1.to(device)
-    # model_2, _ = load_model(args.model)
-    # if args.model_dir_2:
-    #     model_2.load_state_dict(torch.load(model_dir_2))
-    # model_2.to(device)
+    model.to(device)
 
     with torch.no_grad():
+        model.eval()
+        result=[]
+        output_list = []
+        for test_dataloader, i in test_dataloader_list:
+            partial_result = []
+            for images in tqdm(test_dataloader):
+                images = images.float().to(device)
 
-        for model in model_list:
-            model.eval()
-        # model_1.eval()
-        # model_2.eval()
-        result = []
-        for images in tqdm(test_dataloader):
-            images = images.float().to(device)
-
-            output_list = []
-
-            for model in model_list:
                 output = model(images)['out']
+
+                # rotate to original
+                output = torch.rot90(output, k=4-i, dims=(2, 3))   
                 output = torch.sigmoid(output)
-                output_list.append(output)
+                partial_result.append(output)
 
-            avg_output = torch.zeros_like(output_list[0]).to(device)
-            for output in output_list:
-                avg_output += output
-            avg_output /= len(output_list)
-            avg_output = avg_output.cpu().numpy()
+            output_list.append(partial_result)
+        
+        avg_output = torch.zeros_like(output_list[0]).to(device)
+        for output in output_list:
+            avg_output += output
+        avg_output /= len(output_list)
+        avg_output = avg_output.cpu().numpy()
+        masks = (avg_output > args.mask_ratio).astype(np.uint8).astype(np.float32)
 
-            #
-            # outputs_1 = model_1(images)['out']
-            # outputs_1 = torch.sigmoid(outputs_1)
-            # # outputs_1 = outputs_1.cpu().numpy()
-            #
-            # outputs_2 = model_2(images)['out']
-            # outputs_2 = torch.sigmoid(outputs_2)
-            # # outputs_2 = outputs_2.cpu().numpy()
-            #
-            # # avg
-            # outputs = (outputs_1 + outputs_2) / 2
-            # outputs = outputs.cpu().numpy()
-
-            masks = (avg_output > args.mask_ratio).astype(np.uint8).astype(np.float32)
-
-            for i in range(len(images)):
-                mask_rle = rle_encode(masks[i])
-                if mask_rle == '': # 예측된 건물 픽셀이 아예 없는 경우 -1
-                    result.append(-1)
-                else:
-                    result.append(mask_rle)
-
+        for i in range(len(images)):
+            mask_rle = rle_encode(masks[i])
+            if mask_rle == '': # 예측된 건물 픽셀이 아예 없는 경우 -1
+                result.append(-1)
+            else:
+                result.append(mask_rle)
+                        
     submit = pd.read_csv('./sample_submission.csv')
     submit['mask_rle'] = result
 
